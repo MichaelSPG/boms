45e930e71b534dbc1c278f6dcfaa5ce5144d4fcd
ivarboms <ivarboms@gmail.com>
2011-10-24 01:28:12 +0200

    Significantly refactored the whole class. Instead of the entity being owned by a scene node, the entity is now the top level class, and it owns an instance of the new transform class. Replaced templated attach/detatch/getComponent functions with normal functions to reduce complexity (and getting the right type for template arguments was difficult at times). The entity now owns all of its components and will delete them when the entity is destroyed.

--- bsEntity.cpp
+++ bsEntity.cpp
@@ -1,26 +1,271 @@
 #include "StdAfx.h"
 
+#include <Common/Base/hkBase.h>
+#include <Physics/Dynamics/hkpDynamics.h>
+#include <Physics/Dynamics/Entity/hkpRigidBody.h>
+
 #include "bsEntity.h"
-#include "bsSceneNode.h"
+#include "bsText3D.h"
+#include "bsAssert.h"
+#include "bsLight.h"
+#include "bsLineRenderer.h"
+#include "bsCamera.h"
+#include "bsConvert.h"
+#include "bsScene.h"
 
 
-bsEntity::bsEntity(bsSceneNode* owner)
-	: mOwner(owner)
+#pragma warning(push)
+//bsTransform's constructor does not use 'this' for anything but a reference assignment,
+//and 'this' does not have a vtable, so no undefined/unsafe behavior is occuring.
+#pragma warning(disable:4355)// warning C4355: 'this' : used in base member initializer list
+
+bsEntity::bsEntity()
+	: mTransform(this)
 	, mLineRenderer(nullptr)
 	, mLight(nullptr)
 	, mCamera(nullptr)
-	, mText3D(nullptr)
-	, mRigidBody(nullptr)
+	, mTextRenderer(nullptr)
+	, mRigidBody()
+	, mScene(nullptr)
+	, mSceneID(~0u)
 {
+	mBoundingSphere.positionAndRadius = XMVectorSet(0.0f, 0.0f, 0.0f, FLT_MAX);
 
+	BS_ASSERT2(((uintptr_t)this) % 16 == 0, "bsEntity must be 16 byte aligned!");
 }
+#pragma warning(pop)
 
 bsEntity::~bsEntity()
 {
-	
+	if (mScene)
+	{
+		mScene->removeEntity(*this);
+	}
+
+	//Delete all child entities.
+	const std::vector<bsTransform*>& children = mTransform.getChildren();
+	for (unsigned int i = 0; i < children.size(); ++i)
+	{
+		delete &children[i]->getEntity();
+	}
+
+	//Delete all owned components.
+	delete mLineRenderer;
+	delete mLight;
+	delete mCamera;
+	delete mTextRenderer;
 }
 
-void bsEntity::syncRigidBodyWithOwner()
+void bsEntity::attachRigidBody(hkpRigidBody& rigidBody)
 {
-	mOwner->setTransformFromRigidBody();
+	hkpWorld* world = rigidBody.getWorld();
+	if (world)
+	{
+		world->markForWrite();
+	}
+
+	BS_ASSERT2(mRigidBody == nullptr, "Trying to attach a rigid body, but a rigid"
+		" body is already attached");
+	BS_ASSERT2(!rigidBody.hasProperty(BSPK_ENTITY_POINTER), "Trying to attach a"
+		" rigid body, but it is still attached to a different entity");
+
+	mRigidBody = &rigidBody;
+	mRigidBody->setProperty(BSPK_ENTITY_POINTER, hkpPropertyValue(this));
+
+	//Set the rigid body's position and rotation to the same as this entity's position
+	//and rotation.
+	const hkVector4 position(bsMath::toHK(mTransform.getPosition()));
+	hkQuaternion rotation;
+	rotation.m_vec = bsMath::toHK(mTransform.getRotation());
+
+	mRigidBody->setPositionAndRotation(position, rotation);
+
+	if (world)
+	{
+		world->unmarkForWrite();
+	}
+}
+
+void bsEntity::attachMesh(const bsSharedMesh& mesh)
+{
+	BS_ASSERT2(!mMesh, "Trying to attach a mesh, but a mesh is already attached");
+
+	mMesh = mesh;
+}
+
+void bsEntity::attachLight(bsLight& light)
+{
+	BS_ASSERT2(mLight == nullptr, "Trying to attach a light, but a light is already attached");
+
+	mLight = &light;
+}
+
+void bsEntity::attachLineRenderer(bsLineRenderer& lineRenderer)
+{
+	BS_ASSERT2(mLineRenderer == nullptr, "Trying to attach a line renderer, but a line"
+		" renderer is already attached");
+
+	mLineRenderer = &lineRenderer;
+}
+
+void bsEntity::attachCamera(bsCamera& camera)
+{
+	BS_ASSERT2(mCamera == nullptr, "Trying to attach a camera, but a camera is already attached");
+
+	mCamera = &camera;
+	mCamera->setEntity(this);
+}
+
+void bsEntity::attachTextRenderer(bsText3D& textRenderer)
+{
+	BS_ASSERT2(mTextRenderer == nullptr, "Trying to attach a text renderer, but a text"
+		" renderer is already attached");
+
+	mTextRenderer = &textRenderer;
+}
+
+
+
+void bsEntity::detachRigidBody()
+{
+	BS_ASSERT2(mRigidBody != nullptr, "Trying to detach a rigid body, but no rigid"
+		" body is attached");
+
+	mRigidBody->removeProperty(BSPK_ENTITY_POINTER);
+	mRigidBody = nullptr;
+}
+
+void bsEntity::detachMesh()
+{
+	BS_ASSERT2(mMesh, "Trying to detach a mesh, but no mesh is attached");
+
+	mMesh.reset();
+}
+
+void bsEntity::detachLight()
+{
+	BS_ASSERT2(mLight != nullptr, "Trying to detach a light, but no light is attached");
+
+	delete mLight;
+	mLight = nullptr;
+}
+
+void bsEntity::detachLineRenderer()
+{
+	BS_ASSERT2(mLineRenderer != nullptr, "Trying to detach a line renderer,"
+		" but no line renderer is attached");
+
+	delete mLineRenderer;
+	mLineRenderer = nullptr;
+}
+
+void bsEntity::detachCamera()
+{
+	BS_ASSERT2(mCamera != nullptr, "Trying to detach a camera, but no camera is attached");
+
+	delete mCamera;
+	mCamera = nullptr;
+}
+
+void bsEntity::detachTextRenderer()
+{
+	BS_ASSERT2(mTextRenderer != nullptr, "Trying to detach a text renderer, but no text"
+		" renderer is attached");
+
+	delete mTextRenderer;
+	mTextRenderer = nullptr;
+}
+
+
+
+
+bsTransform& bsEntity::getTransform()
+{
+	return mTransform;
+}
+
+const bsTransform& bsEntity::getTransform() const
+{
+	return mTransform;
+}
+
+hkpRigidBody* bsEntity::getRigidBody()
+{
+	return mRigidBody;
+}
+
+const hkpRigidBody* bsEntity::getRigidBody() const
+{
+	return mRigidBody;
+}
+
+bsSharedMesh& bsEntity::getMesh()
+{
+	return mMesh;
+}
+
+const bsSharedMesh& bsEntity::getMesh() const
+{
+	return mMesh;
+}
+
+bsLight* bsEntity::getLight()
+{
+	return mLight;
+}
+
+const bsLight* bsEntity::getLight() const
+{
+	return mLight;
+}
+
+bsLineRenderer* bsEntity::getLineRenderer()
+{
+	return mLineRenderer;
+}
+
+const bsLineRenderer* bsEntity::getLineRenderer() const
+{
+	return mLineRenderer;
+}
+
+bsCamera* bsEntity::getCamera()
+{
+	return mCamera;
+}
+
+const bsCamera* bsEntity::getCamera() const
+{
+	return mCamera;
+}
+
+bsText3D* bsEntity::getTextRenderer()
+{
+	return mTextRenderer;
+}
+
+const bsText3D* bsEntity::getTextRenderer() const
+{
+	return mTextRenderer;
+}
+
+void bsEntity::addedToScene(bsScene& scene, unsigned int id)
+{
+	BS_ASSERT2(mScene == nullptr, "Entity was added to a scene, but it is already in a scene");
+
+	mScene = &scene;
+	mSceneID = id;
+}
+
+void bsEntity::removedFromScene(bsScene& scene)
+{
+	BS_ASSERT2(mScene == &scene, "Entity was removed from a scene it was not a part of");
+
+	const std::vector<bsTransform*>& children = mTransform.getChildren();
+	for (unsigned int i = 0; i < children.size(); ++i)
+	{
+		mScene->removeEntity(children[i]->getEntity());
+	}
+
+	mScene = nullptr;
 }
--- bsEntity.h
+++ bsEntity.h
@@ -1,64 +1,115 @@
 #pragma once
 
-#include <memory>
+#include <memory>//std::shared_ptr
+#include <stdlib.h>//_aligned_malloc/_aligned_free
 
-#include "bsScene.h"
+#include <Common/Base/hkBase.h>
+#include <Common/Base/Types/hkRefPtr.h>
+
+#include "bsTransform.h"
+#include "bsCollision.h"
 
 class hkpRigidBody;
 
 class bsMesh;
-class bsLine3D;
+class bsLineRenderer;
 class bsLight;
-class bsSceneNode;
 class bsCamera;
 class bsText3D;
+class bsScene;
+
+typedef std::shared_ptr<bsMesh> bsSharedMesh;
 
 
-class bsEntity
+/*	Entities are used to represent an object in 3D space which can have multiple components
+	attached to it.
+	For example, an entity can be used to represent a car by attaching the car mesh,
+	an exhaust particle effect and an engine sound to it.
+
+	After creating an entity, add it to a scene to make it active.
+
+	An entity will own all of its components (except for reference counted components),
+	and will delete them when the entity is deleted.
+*/
+__declspec(align(16)) class bsEntity
 {
 public:
-	explicit bsEntity(bsSceneNode* owner);
+	inline void* operator new(size_t)
+	{
+		return _aligned_malloc(sizeof(bsEntity), 16);
+	}
+	inline void operator delete(void* p)
+	{
+		_aligned_free(p);
+	}
+
+
+	bsEntity();
 
 	~bsEntity();
 
 
 	/*	Attaches a component to this entity.
 	*/
-	template <typename T>
-	inline void attach(T component);
+	void attachRigidBody(hkpRigidBody& rigidBody);
+	void attachMesh(const bsSharedMesh& mesh);
+	void attachLight(bsLight& light);
+	void attachLineRenderer(bsLineRenderer& lineRenderer);
+	void attachCamera(bsCamera& camera);
+	void attachTextRenderer(bsText3D& textRenderer);
 
-	/*	Detaches a component from this entity.
-		This will delete the component. If you want to keep the component around, set it
-		to null instead.
+	/*	Detatches a component from this entity. This deletes the component.
 	*/
-	template <typename T>
-	inline void detach();
+	void detachRigidBody();
+	void detachMesh();
+	void detachLight();
+	void detachLineRenderer();
+	void detachCamera();
+	void detachTextRenderer();
+
+	/*	Read-only access to components.
+		If the component does not exist, null is returned.
+	*/
+	const bsTransform& getTransform() const;
+	const hkpRigidBody* getRigidBody() const;
+	const bsSharedMesh& getMesh() const;
+	const bsLight* getLight() const;
+	const bsLineRenderer* getLineRenderer() const;
+	const bsCamera* getCamera() const;
+	const bsText3D* getTextRenderer() const;
+
+	/*	Read/write access to components.
+		If the component does not exist, null is returned.
+	*/
+	bsTransform& getTransform();
+	hkpRigidBody* getRigidBody();
+	bsSharedMesh& getMesh();
+	bsLight* getLight();
+	bsLineRenderer* getLineRenderer();
+	bsCamera* getCamera();
+	bsText3D* getTextRenderer();
 
 
-	template <typename T>
-	inline T getComponent();
+
+	void addedToScene(bsScene& scene, unsigned int id);
+	void removedFromScene(bsScene& scene);
 
 
-	inline bsSceneNode* getOwner() const
-	{
-		return mOwner;
-	}
+	//private:
+	//This entity's unique ID for this scene.
+	unsigned int	mSceneID;
+	bsTransform		mTransform;
 
+	bsCollision::Sphere		mBoundingSphere;
 
-
-//private:
-	void syncRigidBodyWithOwner();
-
-
-	bsSceneNode*	mOwner;
-
-	std::shared_ptr<bsMesh>	mMesh;
-	bsLine3D*				mLineRenderer;
+	bsSharedMesh			mMesh;
+	bsLineRenderer*			mLineRenderer;
 	bsLight*				mLight;
 	bsCamera*				mCamera;
-	bsText3D*				mText3D;
+	bsText3D*				mTextRenderer;
 
-	hkpRigidBody*			mRigidBody;
+	hkRefPtr<hkpRigidBody>	mRigidBody;
+
+	//The scene in which this entity exists, or null if it's not in a scene.
+	bsScene*		mScene;
 };
-
-#include "bsEntity.inl"
