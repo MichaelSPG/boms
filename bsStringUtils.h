5191140f24dbcf67dab832669d47dc604e07c3a0
ivarboms <ivarboms@gmail.com>
2011-10-29 02:44:14 +0200

    Added functions for converting byte count to a string.

--- bsStringUtils.h
+++ bsStringUtils.h
@@ -54,4 +54,61 @@ inline std::string toStringWithPrecision(const T& t, const unsigned int precisio
 	ss << t;
 	return ss.str();
 }
+
+/*	Converts a number representing a number of bytes to a string.
+
+	The number 1024 will be converted to "1.0 KB", 590558003 to "563.2 MB", 3 to "3 bytes",
+	and so on. Units other than bytes are representet with numbers having 1 decimal place.
+	The maximum unit is GB. Values over 1 GB are always specified in GB.
+
+	The parameter 'buffer' needs to be able to fit the entire converted string.
+	11 bytes is enough for anything up to 999.9 GB.
+*/
+template <typename SizeType, size_t Size>
+void byteToString(SizeType bytes, char (&buffer)[Size])
+{
+	static_assert(std::is_integral<SizeType>::value, "SizeType must be an integer");
+	static_assert(Size >= 11, "Buffer is too small, must be able to hold at least 11 characters");
+
+	if (bytes >= 1024 * 1024 * 1024)// >= 1 GB
+	{
+		sprintf(buffer, "%.1f GB", bytes / (1024.0 * 1024.0 * 1024.0));
+	}
+	else if (bytes >= 1024 * 1024)// >= 1 MB
+	{
+		sprintf(buffer, "%.1f MB", bytes / (1024.0 * 1024.0));
+	}
+	else if (bytes >= 1024)// >= 1 KB
+	{
+		sprintf(buffer, "%.1f KB", bytes / 1024.0);
+	}
+	else// < 1 KB
+	{
+		sprintf(buffer, "%u bytes", bytes);
+	}
+}
+
+template <typename SizeType, size_t Size>
+void byteToStringWide(SizeType bytes,  wchar_t(&buffer)[Size])
+{
+	static_assert(std::is_integral<SizeType>::value, "SizeType must be an integer");
+	static_assert(Size >= 11, "Buffer is too small, must be able to hold at least 11 characters");
+
+	if (bytes >= 1024 * 1024 * 1024)// >= 1 GB
+	{
+		swprintf(buffer, Size, L"%.1f GB", bytes / (1024.0 * 1024.0 * 1024.0));
+	}
+	else if (bytes >= 1024 * 1024)// >= 1 MB
+	{
+		swprintf(buffer, Size, L"%.1f MB", bytes / (1024.0 * 1024.0));
+	}
+	else if (bytes >= 1024)// >= 1 KB
+	{
+		swprintf(buffer, Size, L"%.1f KB", bytes / 1024.0);
+	}
+	else// < 1 KB
+	{
+		swprintf(buffer, Size, L"%u bytes", bytes);
+	}
+}
 };
