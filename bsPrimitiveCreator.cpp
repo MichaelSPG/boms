47b441ec96d7d1f37770a4393b675f8413d3a4b7
ivarboms <ivarboms@gmail.com>
2011-11-08 02:08:14 +0100

    Added support for making capsules and cylinders in the primitive creator. Updated existing functions to be more const correct, added some error checking, and computing of accurate inertia tensors, mass and center of mass.

--- bsPrimitiveCreator.cpp
+++ bsPrimitiveCreator.cpp
@@ -7,6 +7,9 @@
 #include <Physics/Collide/Shape/Convex/Sphere/hkpSphereShape.h>
 #include <Physics/Collide/Shape/Convex/Box/hkpBoxShape.h>
 #include <Physics/Collide/Shape/HeightField/Plane/hkpPlaneShape.h>
+#include <Physics/Collide/Shape/Convex/Capsule/hkpCapsuleShape.h>
+#include <Physics/Collide/Shape/Convex/Cylinder/hkpCylinderShape.h>
+#include <Physics/Utilities/Dynamics/Inertia/hkpInertiaTensorComputer.h>
 #include <Physics/Dynamics/Entity/hkpRigidBody.h>
 
 #include "bsEntity.h"
@@ -14,10 +17,13 @@
 #include "bsTextureCache.h"
 #include "bsMath.h"
 #include "bsMeshRenderer.h"
+#include "bsAssert.h"
 
 
-bsEntity* bsPrimitiveCreator::createSphere(float radius)
+bsEntity* bsPrimitiveCreator::createSphere(float radius) const
 {
+	BS_ASSERT2(radius > 0.0f, "A sphere's radius must be positive and non-zero");
+
 	bsEntity* entity = new bsEntity();
 	entity->attachMeshRenderer(*new bsMeshRenderer(mMeshCache.getMesh("sphere_1m_d.bsm"),
 		mTextureCache.getDefaultTexture()));
@@ -26,6 +32,12 @@ bsEntity* bsPrimitiveCreator::createSphere(float radius)
 	rbCinfo.m_shape = new hkpSphereShape(radius);
 	rbCinfo.m_motionType = hkpMotion::MOTION_SPHERE_INERTIA;
 
+	//Compute sphere's mass properties.
+	hkpMassProperties massProperties;
+	hkpInertiaTensorComputer::computeShapeVolumeMassProperties(rbCinfo.m_shape,
+		rbCinfo.m_mass, massProperties);
+	rbCinfo.setMassProperties(massProperties);
+
 	entity->attachRigidBody(*new hkpRigidBody(rbCinfo));
 
 	entity->getTransform().setLocalScaleUniform(radius * 2.0f);
@@ -33,8 +45,15 @@ bsEntity* bsPrimitiveCreator::createSphere(float radius)
 	return entity;
 }
 
-bsEntity* bsPrimitiveCreator::createBox(const XMVECTOR& halfExtents)
+bsEntity* bsPrimitiveCreator::createBox(const XMVECTOR& halfExtents) const
 {
+	BS_ASSERT2(XMVectorGetX(halfExtents) > 0.0f, "A box' extents must be positive in every"
+		" direction");
+	BS_ASSERT2(XMVectorGetY(halfExtents) > 0.0f, "A box' extents must be positive in every"
+		" direction");
+	BS_ASSERT2(XMVectorGetZ(halfExtents) > 0.0f, "A box' extents must be positive in every"
+		" direction");
+
 	bsEntity* entity = new bsEntity();
 	entity->attachMeshRenderer(*new bsMeshRenderer(mMeshCache.getMesh("unit_cube.bsm"),
 		mTextureCache.getDefaultTexture()));
@@ -50,6 +69,13 @@ bsEntity* bsPrimitiveCreator::createBox(const XMVECTOR& halfExtents)
 	rbCinfo.m_shape = new hkpBoxShape(halfExt);
 	rbCinfo.m_motionType = hkpMotion::MOTION_BOX_INERTIA;
 
+	//Compute box' mass properties.
+	hkpMassProperties massProperties;
+	hkpInertiaTensorComputer::computeShapeVolumeMassProperties(rbCinfo.m_shape,
+		rbCinfo.m_mass, massProperties);
+	massProperties.scaleToDensity(1.0f);
+	rbCinfo.setMassProperties(massProperties);
+
 	entity->attachRigidBody(*new hkpRigidBody(rbCinfo));
 
 	entity->getTransform().setLocalScale(XMVectorScale(halfExtents, 2.0f));
@@ -57,14 +83,22 @@ bsEntity* bsPrimitiveCreator::createBox(const XMVECTOR& halfExtents)
 	return entity;
 }
 
-bsEntity* bsPrimitiveCreator::createPlane(const XMVECTOR& halfExtents)
+bsEntity* bsPrimitiveCreator::createPlane(const XMVECTOR& halfExtents) const
 {
+	BS_ASSERT2(XMVectorGetX(halfExtents) > 0.0f, "A plane's extents must be positive in"
+		" both X and Z");
+	BS_ASSERT2(XMVectorGetZ(halfExtents) > 0.0f, "A plane's extents must be positive in"
+		" both X and Z");
+
 	bsEntity* entity = new bsEntity();
 	entity->attachMeshRenderer(*new bsMeshRenderer(mMeshCache.getMesh("plane_1m.bsm"),
 		mTextureCache.getDefaultTexture()));
 
+	XMFLOAT4A halfExtents4A;
+	XMStoreFloat4A(&halfExtents4A, halfExtents);
+
 	hkVector4 halfExt;
-	halfExt.load<4>(halfExtents.m128_f32);
+	halfExt.load<4>(&halfExtents4A.x);
 
 	hkpRigidBodyCinfo rbCinfo;
 	//Create plane shape point upwards at origin.
@@ -74,9 +108,97 @@ bsEntity* bsPrimitiveCreator::createPlane(const XMVECTOR& halfExtents)
 	entity->attachRigidBody(*new hkpRigidBody(rbCinfo));
 
 	XMVECTOR scale = XMVectorScale(halfExtents, 2.0f);
-	//Set Y scale to 1 regardless of input.
+	//Set Y scale to 1 regardless of input. Doesn't make sense to scale the Y axis of a
+	//plane as it is infinitely thin.
 	scale = XMVectorSetY(scale, 1.0f);
 	entity->getTransform().setLocalScale(scale);
+
+	return entity;
+}
+
+bsEntity* bsPrimitiveCreator::createCapsule(float height, float radius) const
+{
+	BS_ASSERT2(height > (radius * 2.0f), "The height of a capsule must always be larger"
+		" than its radius");
+	BS_ASSERT2(radius > 0.0f, "The radius of a capsule must be positive");
+
+	//Capsule mesh dimensions: vertex0: (0, 0.5, 0), vertex1: (0, 1.5, 0), radius: 0.5
+
+	bsEntity* entity = new bsEntity();
+	entity->attachMeshRenderer(*new bsMeshRenderer(mMeshCache.getMesh("capsule.bsm"),
+		mTextureCache.getDefaultTexture()));
+
+	const hkVector4 vertexBottom(0.0f, radius, 0.0f);
+	const hkVector4 vertexTop(0.0f, height - radius, 0.0f);
+	//Capsules have no extra convex radius, no need to bake extra radius.
+
+	hkpRigidBodyCinfo rbCinfo;
+	rbCinfo.m_shape = new hkpCapsuleShape(vertexBottom, vertexTop, radius);
+	rbCinfo.m_motionType = hkpMotion::MOTION_DYNAMIC;
+
+	//Compute capsule's mass properties.
+	hkpMassProperties massProperties;
+	hkpInertiaTensorComputer::computeShapeVolumeMassProperties(rbCinfo.m_shape,
+		rbCinfo.m_mass, massProperties);
+	massProperties.scaleToDensity(1.0f);
+	rbCinfo.setMassProperties(massProperties);
+	
+
+	entity->attachRigidBody(*new hkpRigidBody(rbCinfo));
+
+	//Mesh' radius is 0.5, so if radius parameter is 0.5, the scale needs to be 1,
+	//so we multiply the radius by 2 to get the correct scaling factor.
+	const float radius2 = radius * 2.0f;
+	const float halfHeight = height * 0.5f;
+	
+	entity->getTransform().setScale(XMVectorSet(radius2, halfHeight, radius2, 0.0f));
+	//entity->getTransform().scaleUniform(0.9);
+
+	return entity;
+}
+
+bsEntity* bsPrimitiveCreator::createCylinder(float height, float radius) const
+{
+	//hkpCylinderShape::setNumberOfVirtualSideSegments(128);
+
+
+	BS_ASSERT2(height > 0.0f, "The height of a cylinder must be positive");
+	BS_ASSERT2(radius > 0.0f, "The radius of a cylinder must be positive");
+
+
+	//Cylinder mesh dimensions: vertex0: (0, 0, 0), vertex1: (0, 2, 0), radius: 0.5
+
+	bsEntity* entity = new bsEntity();
+	entity->attachMeshRenderer(*new bsMeshRenderer(mMeshCache.getMesh("cylinder.bsm"),
+		mTextureCache.getDefaultTexture()));
+
+	//Bake convex radius into the cylinder's shape.
+	const hkVector4 vertexBottom(0.0f, hkConvexShapeDefaultRadius, 0.0f);
+	const hkVector4 vertexTop(0.0f, height - hkConvexShapeDefaultRadius, 0.0f);
+
+	hkpRigidBodyCinfo rbCinfo;
+	rbCinfo.m_shape = new hkpCylinderShape(vertexBottom, vertexTop,
+		radius - hkConvexShapeDefaultRadius);//Baking convex radius into shape's radius.
+	rbCinfo.m_motionType = hkpMotion::MOTION_DYNAMIC;
+
+	//Compute capsule's mass properties.
+	hkpMassProperties massProperties;
+	hkpInertiaTensorComputer::computeShapeVolumeMassProperties(rbCinfo.m_shape,
+		rbCinfo.m_mass, massProperties);
+	massProperties.scaleToDensity(1.0f);
+	rbCinfo.setMassProperties(massProperties);
+
+	entity->attachRigidBody(*new hkpRigidBody(rbCinfo));
+
+	//Mesh' radius is 0.5, so if radius parameter is 0.5, the scale needs to be 1,
+	//so we multiply the radius by 2 to get the correct scaling factor.
+	const float radius2 = radius * 2.0f;
+	//Since the cylinder mesh' height is 2, height scale of 1 is only correct when height
+	//is 2, so halve it to get the correct scaling factor.
+	const float halfHeight = height * 0.5f;
+
+	entity->getTransform().setScale(XMVectorSet(radius2, halfHeight, radius2, 0.0f));
+	//entity->getTransform().scaleUniform(0.9);
 
 	return entity;
 }
--- bsPrimitiveCreator.h
+++ bsPrimitiveCreator.h
@@ -7,6 +7,12 @@ class bsMeshCache;
 class bsTextureCache;
 
 
+/*	Utility class for making simple geometric objects with both graphical and physical
+	components.
+	
+	All the objects created by this class are dynamic by default unless stated otherwise
+	by comments for a specific function.
+*/
 class bsPrimitiveCreator
 {
 public:
@@ -24,21 +30,56 @@ public:
 		The sphere's position will be at 0,0,0, and the rotation will be identity.
 		Scale will be based on input parameter.
 	*/
-	bsEntity* createSphere(float radius);
+	bsEntity* createSphere(float radius) const;
 
 	/*	Creates an entity with a box mesh and rigid body with extents specified by the parameter.
 		The box' position will be at 0,0,0, and the rotation will be identity.
 		Scale will be based on input parameter.
 	*/
-	bsEntity* createBox(const XMVECTOR& halfExtents);
+	bsEntity* createBox(const XMVECTOR& halfExtents) const;
 
 	/*	Creates an entity with a plane mesh and rigid body with extents equal to input's x
 		and z components.
 		The plane's position will be at 0,0,0, and the rotation will be identity.
 		Scale will be based on input parameter. y and w components are ignored.
 		The plane's normal will be 0,1,0 (pointing straight up).
+		Planes will be created as fixed, you should not try to make them dynamic.
 	*/
-	bsEntity* createPlane(const XMVECTOR& halfExtents);
+	bsEntity* createPlane(const XMVECTOR& halfExtents) const;
+
+	/*	Creates an entity with a capsule mesh and rigid body.
+
+		The height describes the total height of the capsule, while radius describes the
+		radius of the two spheres at the bottom and top of the capsule (and the cylinder
+		between them).
+
+		The height between the center of the capsule's two spheres must always be larger
+		than 2*radius. This is because the difference between the two sphere centers must
+		always be positive.
+
+		The graphical representation of the two spheres on either end of the capsule will
+		not be completely aligned with the physical version when the ratio between height
+		and radius differ much from the original mesh.
+		Because of this, you will get best results when height is approximately 4 times
+		larger than the radius (graphical and physical versions will match 100% at this
+		ratio).
+	*/
+	bsEntity* createCapsule(float height, float radius) const;
+
+	/*	Creates a cylinder with the provided height and radius.
+		
+		The height describes the total height of the cylinder. The lowest part of the
+		cylinder will always be a circle with center (0,0,0) in local space, while the
+		highest part of it will be a circle with center (0,height,0).
+
+		Height and radius must always be positive.
+
+		Dynamic cylinders do have artifacts when moving, as they may be simulated as
+		capsules depending on the environment, so edges may clip with other objects.
+		For more info, see Physics\Collide\Shape\Convex\Cylinder\hkpCylinderShape.h
+	*/
+	bsEntity* createCylinder(float height, float radius) const;
+
 
 private:
 	bsPrimitiveCreator& operator=(const bsPrimitiveCreator&);
