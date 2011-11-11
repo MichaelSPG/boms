2d0773ff00b7608a5ce275810f5167470aee75cf
ivarboms <ivarboms@gmail.com>
2011-11-11 01:50:56 +0100

    Added functions to generate wireframe for boxes and cylinders.

--- bsGeometryUtils.cpp
+++ bsGeometryUtils.cpp
@@ -12,6 +12,11 @@
 #include <Common/Visualize/hkDebugDisplay.h>
 #include <Common/Base/Types/Geometry/Aabb/hkAabb.h>
 #include <Common/Base/Types/Geometry/Sphere/hkSphere.h>
+#include <Common/Visualize/Shape/hkDisplayCylinder.h>
+#include <Common/Visualize/Shape/hkDisplayAABB.h>
+#include <Common/Visualize/Shape/hkDisplayGeometry.h>
+#include <Common/Visualize/hkDebugDisplay.h>
+#include <Common/Visualize/Shape/hkDisplayBox.h>
 
 #include "bsLineRenderer.h"
 #include "bsConvert.h"
@@ -172,6 +177,123 @@ void createLinesFromSphere(const XMFLOAT3& sphereCenter, float radius,
 			points.push_back(point);
 			points.push_back(point);
 		}
+		points.push_back(firstPoint);
+	}
+
+
+	lineRendererOut->addPoints(points.data(), points.size());
+}
+
+void createLinesFromBox(const XMFLOAT3& halfExtents, bsLineRenderer* lineRendererOut)
+{
+	const XMFLOAT3 corners[8] =
+	{
+		XMFLOAT3(-halfExtents.x, -halfExtents.y, -halfExtents.z), //Left bottom back
+		XMFLOAT3(-halfExtents.x, -halfExtents.y,  halfExtents.z), //Left bottom front
+		XMFLOAT3(-halfExtents.x,  halfExtents.y, -halfExtents.z), //Left top back
+		XMFLOAT3(-halfExtents.x,  halfExtents.y,  halfExtents.z), //Left top front
+
+		XMFLOAT3( halfExtents.x, -halfExtents.y, -halfExtents.z), //Right bottom back
+		XMFLOAT3( halfExtents.x, -halfExtents.y,  halfExtents.z), //Right bottom front
+		XMFLOAT3( halfExtents.x,  halfExtents.y, -halfExtents.z), //Right top back
+		XMFLOAT3( halfExtents.x,  halfExtents.y,  halfExtents.z), //Right top front
+	};
+
+	const XMFLOAT3 lines[24] =
+	{
+		//Top
+		corners[2], corners[6], //Left back - right back
+		corners[2], corners[3], //Left back - left front
+		corners[3], corners[7], //Left front - right front
+		corners[7], corners[6], //Right front - right back
+
+		//Bottom
+		corners[0], corners[4], //Left back - right back
+		corners[0], corners[1], //Left back - left front
+		corners[1], corners[5], //Left front - right front
+		corners[5], corners[4], //Right front - right back
+
+		//Top to bottom
+		corners[2], corners[0], //Left back
+		corners[6], corners[4], //Right back
+		corners[3], corners[1], //Left front
+		corners[7], corners[5], //Right front
+	};
+
+	lineRendererOut->addPoints(lines, 24);
+}
+
+void createLinesFromCylinder(float height, float radius, unsigned int numSides,
+	bsLineRenderer* lineRendererOut)
+{
+	std::vector<XMFLOAT3> points;
+	//6 = the number of points needed per side (1 line on each of top/bottom lines + 
+	//1 line to connect the points on top/bottom lines).
+	points.reserve(numSides * 6);
+
+	//How much to step with per loop iteration so that currentDelta will interpolate
+	//between 0 and 2*pi.
+	const float delta = (XM_PI * 2) / numSides;
+
+
+	/// Generate circle around bottom Y axis.
+	{
+		float currentDelta = 0.0f;
+
+		//The first point needs to be stored so that the last line can connect to it.
+		const XMFLOAT3 firstPoint(sin(currentDelta) * radius, 0.0f,
+			-cos(currentDelta) * radius);
+
+		points.push_back(firstPoint);
+		currentDelta += delta;
+
+		XMFLOAT3 previousPoint(firstPoint);
+
+		//Point being used to generate interpolated points.
+		XMFLOAT3 point;
+
+		for (unsigned int i = 1; i < numSides; ++i, currentDelta += delta)
+		{
+			//Point on bottom circle.
+			point.x = sin(currentDelta) * radius;
+			point.y = 0.0f;
+			point.z = -cos(currentDelta) * radius;
+
+			//End of previous line segment going around bottom circle.
+			points.push_back(point);
+			{
+				//Line segment going from a point on the bottom circle to the equivalent
+				//point on the top circle.
+				XMFLOAT3 topPt(point);
+				topPt.y = height;
+				points.push_back(point);
+				points.push_back(topPt);
+
+				//Segment of circle going around top circle.
+				XMFLOAT3 previousTopPt(previousPoint);
+				previousTopPt.y = height;
+				points.push_back(previousTopPt);
+				points.push_back(topPt);
+			}
+			//Start new line segment going around bottom circle.
+			points.push_back(point);
+
+			previousPoint = point;
+		}
+		points.push_back(firstPoint);
+
+
+		//Final line segment of top circle to close it.
+		XMFLOAT3 topFirst(firstPoint);
+		topFirst.y = height;
+		XMFLOAT3 topLast(point);
+		topLast.y = height;
+
+		points.push_back(topLast);
+		points.push_back(topFirst);
+
+		//Final vertical line going from bottom start point to equivalent point on top circle.
+		points.push_back(topFirst);
 		points.push_back(firstPoint);
 	}
 
--- bsGeometryUtils.h
+++ bsGeometryUtils.h
@@ -1,16 +1,18 @@
 #pragma once
 
-
 #include <Common/Base/hkBase.h>
 #include <Physics/Collide/Shape/hkpShape.h>
-#include <Physics/Collide/Shape/Convex/Box/hkpBoxShape.h>
-#include <Common/Base/Types/Geometry/Aabb/hkAabb.h>
+
+#include <xnamath.h>
 
 class bsLineRenderer;
 
 
 /*	Geometric utilities used to create geometry Havok objects.
 	Not yet functional.
+
+	These functions do NOT call build() on the provided line renderers, they only add
+	a number of points to them.
 */
 namespace bsGeometry
 {
@@ -25,5 +27,15 @@ void createLinesFromShape(const hkpShape* const shape, bsLineRenderer* line3D);
 */
 void createLinesFromSphere(const XMFLOAT3& sphereCenter, float radius,
 	unsigned int linesPerAxis, bsLineRenderer* lineRendererOut);
+
+void createLinesFromBox(const XMFLOAT3& halfExtents, bsLineRenderer* lineRendererOut);
+
+/*	Creates lines to visualize a cylinder. The cylinder's origin will be at 0,0,0.
+	numSides describes the cylinder's tesselation, and its value will increase
+	both the number of sides of the cylinder and the top/bottom circles' amount of detail.
+	
+*/
+void createLinesFromCylinder(float height, float radius, unsigned int numSides,
+	bsLineRenderer* lineRendererOut);
 
 } // namespace bsGeometry
